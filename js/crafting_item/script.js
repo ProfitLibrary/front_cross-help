886c0fb260984835e277e390642daf3392b90837
Egor1496 <Egor1496Work@mail.ru>
2018-11-14 18:14:57 +0300

    Правки

--- js/crafting_item/script.js
+++ js/crafting_item/script.js
@@ -2,6 +2,7 @@
 $(document).ready(function(){
   var $MasPersent = $("[data-percent]");
   for (var i = 0; i < $($MasPersent).length; i++) { //перебираем все эл-ты .percent
+    console.log(i);
     var $value = $($("[data-percent]")[i]).attr("data-percent"); // массив значений атрибута data-percent
     $($($MasPersent[i]).find(".progress_bar__filling")[parseInt($value / 10)]).css( "width", (($value % 10)*10)+"%"); // закрашиваем нужную ячейку на n процентов 
     for (var j = parseInt($value / 10) + 1; j < 10; j++) {
@@ -11,9 +12,11 @@ $(document).ready(function(){
   $(".item_peculiarity__more").click(function() {
     if($(".item_main_characteristics").css("visibility") == "hidden"){
       $(".item_main_characteristics").css("visibility", "visible");
+      $(".item_peculiarity__more").addClass("item_peculiarity__more--active");
     }
     else{
       $(".item_main_characteristics").css("visibility", "hidden");
+      $(".item_peculiarity__more").removeClass("item_peculiarity__more--active");
     }
   });
 });
